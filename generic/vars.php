3bbb9ca9deb00ea89fc852f1a9d43a09a41829a2
kaschunuss <kaschunuss26@googlemail.com>
2018-10-20 12:01:41 +0200

    db, gitignore

---
+++ generic/vars.php
@@ -0,0 +1,7 @@
+<?php
+
+$__DBNAME = "groupup";
+$__DBHOST = "localhost";
+$__DBPASS = "1Secure2";
+$__DBUSER = "groupup";
+
---
+++ handler/db.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once __DIR__ . '/../generic/vars.php';
+
+$__DB = new mysqli("$__DBHOST", "$__DBUSER", "$__DBPASS", "$__DBNAME");
