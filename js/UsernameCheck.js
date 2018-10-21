60c3079f3aae2240c903847fcbf1bfcf267ddc9e
Mondanzo <hamburg.karl.nehring@gmail.com>
2018-10-21 03:05:04 +0200

    Added Username Check (Still ALPHA) :hammer:

---
+++ js/UsernameCheck.js
@@ -0,0 +1,38 @@
+let usernameInput;
+let timeoutID;
+let button;
+let username;
+
+window.onload = () => {
+    button = document.getElementById("usernameCheckButton");
+    usernameInput = document.getElementById("usernameInput");
+    usernameInput.onkeyup = () => {
+        button.classList.add("hidden");
+        console.log("Waiting 2000ms");
+        clearTimeout(timeoutID);
+        timeoutID = setTimeout(checkUsername, 2000);
+    };
+    button.onclick = () => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("post", "handler/username.php");
+        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        xhr.send("username=" + username);
+    }
+};
+
+function checkUsername(){
+    if(usernameInput.value){
+        username = usernameInput.value;
+        let xhr = new XMLHttpRequest();
+        xhr.open("post", "handler/username.php");
+        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4){
+                if(xhr.response == "false"){
+                    button.classList.remove("hidden");
+                }
+            }
+        };
+        xhr.send("action=check&username=" + username);
+    }
+}
--- misc/navbar.php
+++ misc/navbar.php
@@ -5,13 +5,17 @@
 ?>
 
 <!-- Navigation for main page, add page and search page -->
+
+<!-- load script first so login works -->
+<script src="js/UsernameCheck.js"></script>
 <nav class="uk-navbar-container" uk-navbar>
     <div class="uk-navbar-left">
         <a class="uk-navbar-item uk-logo" href="index.php">GroupUp</a>
     </div>
     <div class="uk-navbar-center">
         <div class="uk-navbar-item">
-            <input type="text" class="uk-input" name="username" placeholder="Username" value="<?php echo $_SESSION['username'] ?>">
+            <input type="text" class="uk-input" id="usernameInput" name="username" placeholder="Username" value="<?php echo $_SESSION['username'] ?>">
+            <button class="uk-button uk-button-primary hidden square" id="usernameCheckButton"><span class="uk-icon" uk-icon="icon: check"></span></button>
         </div>
     </div>
     <div class="uk-navbar-right">
