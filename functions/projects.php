f8102b5e4972c41adfd1297892cc051625c0c299
kaschunuss <kaschunuss26@googlemail.com>
2018-10-21 09:53:49 +0200

    members

--- functions/projects.php
+++ functions/projects.php
@@ -1,6 +1,7 @@
 <?php
 
     require_once __DIR__ . "/tags.php";
+    require_once __DIR__ . "/members.php";
     require_once __DIR__ . "/../handler/db.php";
 
     function addProject(string $name, string $description, string $hardware, string $missing, string $problems, int $eventId, array $tags){
@@ -13,13 +14,15 @@ values
             $__DB->real_escape_string($hardware) . "', '" .
             $__DB->real_escape_string($missing) . "', '" .
             $__DB->real_escape_string($problems) . "', " .
-            $__DB->real_escape_string($eventId) . ", 1)";
+            $__DB->real_escape_string($eventId) . ", " .
+            $_SESSION['uid'] . ")";
         $__DB->query($sql);
         $sql_getId = "SELECT id FROM projects where name='" . $__DB->real_escape_string($name) . "' and description='" . $__DB->real_escape_string($description) . "' order by insert_date desc limit 1";
         $projectid = $__DB->query($sql_getId)->fetch_assoc()['id'];
 
         $sql_tagproject = "INSERT INTO tagged values ";
         foreach ($tags as $key => $color){
+            $key = strtoupper($key);
             if($tag = checkTag($key, $color)){
                 $sql_tagproject .= "(" . $projectid . ", " . $tag['id'] . "),";
             }else{
@@ -31,6 +34,7 @@ values
             }
         }
         $sql_tagproject = rtrim($sql_tagproject, ",") . ";";
-        print_r($sql_tagproject);
         $__DB->query($sql_tagproject);
+
+        addMemberToProject($_SESSION['uid'], $projectid);
 }
