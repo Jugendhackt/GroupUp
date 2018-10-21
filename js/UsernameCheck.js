a0bcfb20a28e0a6075e7515ec7aeee4e2de2681c
Mondanzo <hamburg.karl.nehring@gmail.com>
2018-10-21 09:29:27 +0200

    Enter Support for Usernames! :tada: :clap:

--- js/UsernameCheck.js
+++ js/UsernameCheck.js
@@ -5,6 +5,7 @@ let username;
 let spinner;
 
 let currentTest;
+let accept = false;
 
 window.onload = () => {
     button = document.getElementById("usernameCheckButton");
@@ -13,10 +14,17 @@ window.onload = () => {
     usernameInput.onkeyup = () => {
         button.classList.add("hidden");
         spinner.classList.add("hidden");
+        accept = false;
         clearTimeout(timeoutID);
         timeoutID = setTimeout(checkUsername, 2000);
     };
+
+    usernameInput.onenter = () => {
+        button.click();
+    };
     button.onclick = () => {
+        if(!accept)
+            return;
         let xhr = new XMLHttpRequest();
         xhr.open("post", "handler/username.php");
         xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
@@ -39,14 +47,12 @@ function checkUsername(){
         xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
         xhr.onreadystatechange = () => {
             if(xhr.readyState == 4){
-                if(xhr.response === "false"){
-                    button.classList.remove("hidden");
-                    spinner.classList.add("hidden");
-                } else {
+                if(xhr.response === "true"){
                     UIkit.notification("Oh oh! It seems like this name is already taken!");
-                    spinner.classList.add("hidden");
-                    button.classList.remove("hidden");
                 }
+                button.classList.remove("hidden");
+                spinner.classList.add("hidden");
+                accept = true;
             }
         };
         currentTest = xhr;
